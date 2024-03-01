7950d44c94514c788bd1fa18f5ce1bcbd19713be
“hasanabulhasan01” <hasanabulhasan131@gmail.com>
2024-03-02 02:10:43 +0500

    navigation and location work done

--- src/views/Pickup.js
+++ src/views/Pickup.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
-import * as Location from 'expo-location';
+import React, { useState, useEffect } from "react";
+import { View, Text, Button } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 const Pickup = ({ navigation }) => {
   const [location, setLocation] = useState(null);
@@ -9,62 +9,84 @@ const Pickup = ({ navigation }) => {
 
   useEffect(() => {
     (async () => {
-      
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
         return;
       }
+      Location.watchPositionAsync(
+        {
+          accuracy: 6,
+          distanceInterval: 1,
+          timeInterval: 100,
+        },
+        (location) => {
+          console.log("location", location);
+          setLocation(location);
+        }
+      );
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+
     })();
   }, []);
 
-  let text = 'Waiting..';
+  let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
-    text = JSON.stringify(location);
+    text = "Loading...";
   }
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Pickup</Text>
-      <MapView
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-       style={styles.map} />
+      {location && (
+        <MapView
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0031,
+            longitudeDelta: 0.0031,
+          }}
+          style={styles.map}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title={"Your Location"}
+            description={"My Home"}
+          />
+        </MapView>
+      )}
       <View style={styles.buttonContainer}>
-        <Button title="Car Selection" onPress={() => navigation.navigate("CarSelection")} />
+        <Button
+          title="Car Selection"
+          onPress={() => navigation.navigate("CarSelection")}
+        />
       </View>
     </View>
   );
-}
+};
 
 const styles = {
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
     paddingHorizontal: 20,
   },
   text: {
     fontSize: 24,
-    textAlign: 'center',
-    
+    textAlign: "center",
   },
   buttonContainer: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     marginBottom: 40,
   },
   map: {
-    width: '100%',
-    height: '80%',
-
+    width: "100%",
+    height: "80%",
   },
 };
 
